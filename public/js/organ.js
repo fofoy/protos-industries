d3068c3217d9ef6a0293065339f176aebddcbfc3
Maxime Debavelaere <maxime.debavelaere@gmail.com>
2013-02-22 02:02:52 +0100

    Add webgl collada object

--- public/js/organ.js
+++ public/js/organ.js
@@ -1,70 +1,72 @@
 $(function(){
-// set the scene size
+/* 1. SCENE */
+var $container = $('#organ_3d');
 	var WIDTH = 600,
 	    HEIGHT = 500;
-
-	// set some camera attributes
 	var VIEW_ANGLE = 45,
-	    ASPECT = WIDTH / HEIGHT,
-	    NEAR = 0.1,
-	    FAR = 10000;
+	    ASPECT = WIDTH / HEIGHT;
+var scene = new THREE.Scene();
 
-	// get the DOM element to attach to
-	// - assume we've got jQuery to hand
-	var $container = $('#organ_3d');
 
-	// create a WebGL renderer, camera
-	// and a scene
-	var renderer = new THREE.WebGLRenderer();
-	var camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-	                                ASPECT,
-	                                NEAR,
-	                                FAR  );
-	var scene = new THREE.Scene();
+/* 2. CAMERA */
 
-	// the camera starts at 0,0,0 so pull it back
-	camera.position.z = 300;
+var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, 0.1, 1000);
+camera.position.set(0,0,5);
 
-	// start the renderer
-	renderer.setSize(WIDTH, HEIGHT);
 
-	// attach the render-supplied DOM element
-	$container.append(renderer.domElement);
+/* 3. OBJECTS */
 
-	// create the sphere's material
-	var sphereMaterial = new THREE.MeshLambertMaterial(
-	{
-	    color: 0xCC0000
-	});
+var loader = new THREE.ColladaLoader();
+loader.options.convertUpAxis = true;
+loader.load( 'public/models/brain.dae', function (model) {
+	var piano = model.scene;
+	//piano.scale.set(0.2,0.2,0.2);
+	piano.position.set(30,30,-100);
+	piano.rotation.set(Math.PI/8,Math.PI/4,0);
+	scene.add(piano);
+});
 
-	// set up the sphere vars
-	var radius = 50, segments = 16, rings = 16;
+/* 4. LIGHTS */
 
-	// create a new mesh with sphere geometry -
-	// we will cover the sphereMaterial next!
-	var sphere = new THREE.Mesh(
-	   new THREE.SphereGeometry(radius, segments, rings),
-	   sphereMaterial);
+var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1); 
+	directionalLight.position.set(-5,1,10); 
+scene.add(directionalLight);
 
-	// add the sphere to the scene
-	scene.add(sphere);
 
-	// and the camera
-	scene.add(camera);
+/* 5. RENDERER */
 
-	// create a point light
-	var pointLight = new THREE.PointLight( 0xFFFFFF );
+var renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(WIDTH, HEIGHT);
+$container.append(renderer.domElement);
 
-	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
 
-	// add to the scene
-	scene.add(pointLight);
+function render() { 
+	requestAnimationFrame(render);
+	if(rotate){
+		var timer = Date.now() * 0.0015;
+		camera.position.x  = Math.cos(timer) * 65;
+		camera.position.z  = Math.sin(timer) * 65;
+	}
+	camera.lookAt(scene.position); 
+	renderer.render(scene, camera); 
+} 
 
-	// draw!
-	renderer.render(scene, camera);
+render();
+
+
+
+
+/* CONTROL */
+
+var rotate = false;
+
+/*
+document.addEventListener('mousemove', function(e){
+	var mouseX = e.clientX - window.innerWidth/2;
+	camera.position.set(mouseX/10, 0, 15);
+})
+*/
+
 
 
 });
