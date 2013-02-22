14eba37ddb4d00f95bc983ca50c83c9e041d6ef3
Maxime Debavelaere <maxime.debavelaere@gmail.com>
2013-02-22 06:54:43 +0100

    Fix controls webgl

--- public/js/organ.js
+++ public/js/organ.js
@@ -16,7 +16,7 @@ camera.position.set(0,0,-6);
 controls = new THREE.TrackballControls( camera );
 
 controls.rotateSpeed = 3.0;
-controls.zoomSpeed = 1.2;
+controls.zoomSpeed = 0.2;
 controls.panSpeed = 0.8;
 controls.noZoom = false;
 controls.noPan = false;
