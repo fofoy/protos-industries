bc9c3a0e43f0c182c3d73db5ae4f8afd476bb547
Maxime Debavelaere <maxime.debavelaere@gmail.com>
2013-02-22 03:31:20 +0100

    camera webgl

--- public/js/organ.js
+++ public/js/organ.js
@@ -3,7 +3,7 @@ $(function(){
 var $container = $('#organ_3d');
 	var WIDTH = 600,
 	    HEIGHT = 500;
-	var VIEW_ANGLE = 45,
+	var VIEW_ANGLE = 75,
 	    ASPECT = WIDTH / HEIGHT;
 var scene = new THREE.Scene();
 
@@ -11,7 +11,7 @@ var scene = new THREE.Scene();
 /* 2. CAMERA */
 
 var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, 0.1, 1000);
-camera.position.set(0,0,5);
+camera.position.set(0,0,-1);
 
 
 /* 3. OBJECTS */
@@ -19,17 +19,17 @@ camera.position.set(0,0,5);
 var loader = new THREE.ColladaLoader();
 loader.options.convertUpAxis = true;
 loader.load( 'public/models/brain.dae', function (model) {
-	var piano = model.scene;
-	//piano.scale.set(0.2,0.2,0.2);
-	piano.position.set(30,30,-100);
-	piano.rotation.set(Math.PI/8,Math.PI/4,0);
-	scene.add(piano);
+	var organ = model.scene;
+	//organ.scale.set(0.2,0.2,0.2);
+	organ.position.set(30,6,-10);
+	organ.rotation.set(Math.PI/8,Math.PI/4,0);
+	scene.add(organ);
 });
 
 /* 4. LIGHTS */
 
 var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1); 
-	directionalLight.position.set(-5,1,10); 
+	directionalLight.position.set(-5,1,-10); 
 scene.add(directionalLight);
 
 
@@ -39,6 +39,17 @@ var renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(WIDTH, HEIGHT);
 $container.append(renderer.domElement);
 
+document.addEventListener( 'mousemove', onMouseMove, false );
+
+function onMouseMove(event){
+	mouseX = (event.clientX - WIDTH/2) / WIDTH/2;
+	mouseY = (event.clientY - HEIGHT/2) / HEIGHT/2;
+	camera.position.x = Math.sin(mouseX * Math.PI);
+	camera.position.y = - Math.sin(mouseY * Math.PI);
+	camera.lookAt(new THREE.Vector3(0,0,0));
+	camera.lookAt(organ.position); 
+	renderer.render( scene, camera );
+}
 
 function render() { 
 	requestAnimationFrame(render);
