2d85f5bfedc1407f66a0c7cf9247bec11f9314f3
fofoy <florian.quiblier@gmail.com>
2013-02-25 21:33:14 +0100

    Solved database connection problem for mac users

--- app/Models/Organ.php
+++ app/Models/Organ.php
@@ -2,7 +2,7 @@
 class Organ extends Prefab{
 
     function __construct(){
-        F3::set('dB',new DB\SQL('mysql:host='.F3::get('db_host').';port=3306;dbname='.F3::get('db_server'),F3::get('db_user'),F3::get('db_password')));
+        F3::set('dB',new DB\SQL('mysql:host='.F3::get('db_host').';port=3306;dbname='.F3::get('db_server'),F3::get('db_user'),F3::get('db_pw')));
     }
 
     function getAllOrgans(){
